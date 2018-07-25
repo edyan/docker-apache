7c716546b1dcd0f826119adce6ef7683d897e899
Emmanuel Dyan <emmanuel.dyan@inetprocess.com>
2018-07-25 21:59:03 +0200

    Updated to stretch and latest alpine. Fixed permissions issue that was reseted on each restart even if the UID was the same

--- 2.4-slim/run.sh
+++ 2.4-slim/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-sed -i "s/:100:101:apache:/:$APACHE_UID:$APACHE_GID:apache:/g" /etc/passwd
-sed -i "s/:101:apache/:$APACHE_GID:apache/g" /etc/group
-chown -R apache:apache /run/apache2 /var/www
+CURRENT_APACHE_ID=$(id -u apache)
+if [ "$CURRENT_APACHE_ID" != "$APACHE_UID" ]; then
+    echo "Fixing permissions for Apache"
+    sed -i "s/:100:101:apache:/:$APACHE_UID:$APACHE_GID:apache:/g" /etc/passwd
+    sed -i "s/:101:apache/:$APACHE_GID:apache/g" /etc/group
+    chown -R apache:apache /run/apache2 /var/www
+fi
 
 exec /usr/sbin/httpd -DFOREGROUND;
--- 2.4/run.sh
+++ 2.4/run.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-usermod -u $APACHE_UID www-data
-groupmod -g $APACHE_GID www-data
-chown -R www-data:www-data /var/cache/apache2 /var/lib/apache2 /var/www /run/lock/apache2
+CURRENT_APACHE_ID=$(id -u www-data)
+if [ "$CURRENT_APACHE_ID" != "$APACHE_UID" ]; then
+    echo "Fixing permissions for Apache"
+    usermod -u $APACHE_UID www-data
+    groupmod -g $APACHE_GID www-data
+    chown -R www-data:www-data /var/cache/apache2 /var/lib/apache2 /var/www /run/lock/apache2
+fi
 
 exec /usr/sbin/apache2ctl -D FOREGROUND
